b2850590af270ac3a52d152c583be44f9e35f7c8
Iván Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-11-22 20:28:07 +0100

    Creado menu settings (pendiente de programar)

---
+++ godot/Escenas/menu_settings.gd
@@ -0,0 +1,6 @@
+extends Control
+
+
+#Video Settings
+
+
--- godot/Scripts/Menu.gd
+++ godot/Scripts/Menu.gd
@@ -18,3 +18,5 @@ func _on_play_button_pressed():
 func _on_exit_pressed():
 	get_tree().quit()
 
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Escenas/menu_settings.tscn")
