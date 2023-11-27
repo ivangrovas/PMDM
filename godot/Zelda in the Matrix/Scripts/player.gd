0b50fb180d90c7982602846172fbbf224b32cfcb
Ivan Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-11-27 10:46:35 +0100

    Se ha reducido el codigo de animaciones, quitando animaciones que no se van a realizar finalmente, menú de settings diseñado (pendiente programar)

--- godot/Zelda in the Matrix/Scripts/player.gd
+++ godot/Zelda in the Matrix/Scripts/player.gd
@@ -2,13 +2,12 @@ extends CharacterBody2D
 
 
 const speed = 200.0
-@export var jump_velocity : float = -150.0
-@export var double_jump_velocity : float = -100
+@export var jump_velocity : float = -300.0
+@export var double_jump_velocity : float = -200
 
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction : Vector2 = Vector2.ZERO
-var was_in_air : bool = false
 var has_double_jumped : bool = false
 var animation_locked : bool = false
 
@@ -21,20 +20,18 @@ func _ready():
 
 func update_animation():
 	animation_tree.set("parameters/Move/blend_position", direction.x)
+	if direction.x < 0:
+		animated_sprite.flip_h = false
+	elif direction.x > 0:
+		animated_sprite.flip_h = true
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		was_in_air = true
 	else:
 		has_double_jumped = false
-		
-		if was_in_air == true:
-			land()
-			
-		was_in_air = false
 	
 	if Input.is_action_just_pressed("Jump"):
 		if is_on_floor():
@@ -46,25 +43,19 @@ func _physics_process(delta):
 		
 	direction = Input.get_vector("ui_left", "ui_right", "up", "down")
 	
-	if direction.x != 0 && animated_sprite.animation != "jump_end":
+	if direction.x != 0:
 		velocity.x = direction.x * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.x = 0
 
 	move_and_slide()
 	update_animation()
-	
-func land():
-	animated_sprite.play("jump_end")
-	animation_locked = true
 
 func jump():
 	velocity.y = jump_velocity
-	animated_sprite.play("jump_start")
 	animation_locked = true
 
 func double_jump():
 	velocity.y = double_jump_velocity
-	animated_sprite.play("jump_double")
 	animation_locked = true
 	has_double_jumped = true
