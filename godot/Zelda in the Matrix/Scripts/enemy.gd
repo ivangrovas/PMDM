51dd0df7f1c16a9a22488d4976c77699f0464b8c
Iván Grovas <igrovasp22@fpcoruna.afundacion.org>
2023-12-07 14:43:13 +0100

    Ahora el enemigo nos sigue

--- godot/Zelda in the Matrix/Scripts/enemy.gd
+++ godot/Zelda in the Matrix/Scripts/enemy.gd
@@ -1,24 +1,42 @@
 extends CharacterBody2D
 
-
-@export var speed = 50
-
-var velocidad:int = 50
+var speed:int = 50
+var perseguir:bool = false
+var vel_perseguidor:int=100
 
 func _ready():
 	velocity.x=-speed
 
 func _physics_process(delta):
-	if is_on_wall():
-		pass
-		if !$AnimatedSprite2D.flip_h:
-			velocity.x=speed
-		else:
-			velocity.x=-speed
-
+	detector()
+	if !perseguir:
+		if is_on_wall():
+			if !$AnimatedSprite2D.flip_h:
+				velocity.x=speed
+			else:
+				velocity.x=-speed
+			
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h=false
+		elif velocity.x >0:
+			$AnimatedSprite2D.flip_h=true
 	move_and_slide()
+
+func detector():
+	if $right.is_colliding():
+		var obj = $right.get_collider()
+		if obj.is_in_group("player"):
+			perseguir=true
+			velocity.x=vel_perseguidor
+			$AnimatedSprite2D.flip_h=true
+		else: 
+			perseguir=false
 	
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h=false
-	elif velocity.x >0:
-		$AnimatedSprite2D.flip_h=true
+	if $left.is_colliding():
+		var obj = $left.get_collider()
+		if obj.is_in_group("player"):
+			perseguir=true
+			velocity.x= -vel_perseguidor
+			$AnimatedSprite2D.flip_h=false
+		else: 
+			perseguir=false
